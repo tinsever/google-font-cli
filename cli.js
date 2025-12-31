0fe7ebd58efbc191cabaf5829c4fca7f3347f625
Tin Sever <tinsever@Air-von-Tin.fritz.box>
2025-12-31 01:47:58 +0100

    use gwfh instead of google font api

--- cli.js
+++ cli.js
@@ -1,175 +1,150 @@
 #!/usr/bin/env node
 
-'use strict'
+"use strict";
 
-var program = require('commander');
-var colors = require('colors');
-var GoogleFontList = require('./lib/google-font-list');
-var fontList = new GoogleFontList('AIzaSyB1I5eF2kRqqs50DS8qBJtFkCTMMoQLusg');
-var pjson = require('./package.json');
-var ncp = require("copy-paste-win32fix");
+const program = require("commander");
+const pc = require("picocolors");
+const ncp = require("copy-paste-win32fix");
+const GoogleFontList = require("./lib/google-font-list");
+const pjson = require("./package.json");
 
-program
-	.command('search [family...]')
-	.description('Search for a family')
-	.action(function(family){
-		var searchedTerm = family ? family.join(" ") : '';
-		fontList.on('success', function(){
-			searchFont(searchedTerm);
-		})
+const fontList = new GoogleFontList();
 
-	})
+/**
+ * Helper to wrap fontList initialization in a Promise
+ */
+const ensureFontsLoaded = () =>
+  new Promise((resolve) => {
+    if (fontList.loaded) return resolve();
+    console.log(pc.bold(pc.blue("\nDownloading Google Font List...\n")));
+    fontList.on("success", resolve);
+    fontList.on("error", (err) => {
+      console.error(pc.bold(pc.red("Error loading font list!")));
+      console.error(pc.red(err.toString()));
+      process.exit(1);
+    });
+  });
 
-program
-	.command('download <family...>')
-	.option('-d, --dest <folder>', 'Specify where to download')
-	.option('-v, --variants <variants...>', 'Specify variants/weights to install, separated with comma (and no spaces)')
-	.description('Download a font family')
-	.action(function(family, options){
-		var searchedTerm = family.join(" ");
-		var variants = options.variants ? options.variants.split(',') : false;
-		var path = options.dest || false;
-		fontList.on('success', function(){
-			downloadFont(searchedTerm, variants, path);
-		})
-	});
+program.version(pjson.version);
 
 program
-	.command('install <family...>')
-	.option('-v, --variants <variants...>', 'Specify variants/weights to install, separated with comma (and no spaces)')
-	.description('Install a font family')
-	.action(function(family, options){
-		var searchedTerm = family.join(" ");
-		var variants = options.variants ? options.variants.split(',') : false;
-		fontList.on('success', function(){
-			installFont(searchedTerm, variants);
-		})
-	});
+  .command("search [family...]")
+  .description("Search for a font family")
+  .action(async (family) => {
+    await ensureFontsLoaded();
+    const term = family ? family.join(" ") : "";
+    fontList.searchFontByName(term, printFontList);
+  });
 
 program
-	.command('copy <family...>')
-	.option('-v, --variants <variants...>', 'Specify variants/weights to copy clipboard, seperated with comma (and no spaces)')
-	.description('Copy stylesheet link')
-	.action(function(family, options){
-		var searchedTerm = family.join(" ");
-		var variants = options.variants ? options.variants.split(',') : false;
-		fontList.on('success', function(){
-			copyFont(searchedTerm, variants);
-		})
+  .command("download <family...>")
+  .description("Download a font family")
+  .option("-d, --dest <folder>", "Specify destination folder")
+  .option("-v, --variants <variants>", "Variants separated by comma")
+  .action(async (family, options) => {
+    await ensureFontsLoaded();
+    const term = family.join(" ");
+    const variants = options.variants ? options.variants.split(",") : false;
 
-	});
+    fontList.getFontByName(term, (err, filteredList) => {
+      if (err || filteredList.data.length !== 1) {
+        handleMatchError("Download", term, err);
+        return;
+      }
+      filteredList.getFirst().saveAt(variants, options.dest, printResult);
+    });
+  });
 
 program
-	.version(pjson.version);
+  .command("install <family...>")
+  .description("Install a font family to the system")
+  .option("-v, --variants <variants>", "Variants separated by comma")
+  .action(async (family, options) => {
+    await ensureFontsLoaded();
+    const term = family.join(" ");
+    const variants = options.variants ? options.variants.split(",") : false;
+
+    fontList.getFontByName(term, (err, filteredList) => {
+      if (err || filteredList.data.length !== 1) {
+        handleMatchError("Installation", term, err);
+        return;
+      }
+      filteredList.getFirst().install(variants, printResult);
+    });
+  });
+
+program
+  .command("copy <family...>")
+  .description("Copy Google Fonts stylesheet link to clipboard")
+  .option("-v, --variants <variants>", "Variants separated by comma")
+  .action(async (family, options) => {
+    await ensureFontsLoaded();
+    const term = family.join(" ");
+    const variants = options.variants ? options.variants.split(",") : false;
+
+    fontList.getFontByName(term, (err, filteredList) => {
+      if (err || filteredList.data.length !== 1) {
+        handleMatchError("Copy", term, err);
+        return;
+      }
+      const font = filteredList.getFirst();
+      const url = variants
+        ? `${font.cssUrl}:${variants.join(",")}`
+        : font.cssUrl;
+
+      ncp.copy(url, () => {
+        console.log(pc.green(`"${term}" CSS URL copied to clipboard.`));
+      });
+    });
+  });
 
 program.parse(process.argv);
 
-
-// CHECK IF NO COMMANDS ARE PROVIDED AND PRINT HELP IN NONE
-var found = false;
-program.args.forEach(function(arg){
-	if (arg instanceof program.Command )
-		found = true;
-})
-if (!found)
-	program.help();
-
-// HELPERS FUNCTIONS
-
-console.log('\nDonwloading Google Font List....\n'.blue.bold);
-
-fontList.on('error', function(err){
-	printError(err);
-})
-
-function searchFont(searchedTerm) {
-	fontList.searchFontByName(searchedTerm, printFontList)
+// Handle empty commands
+if (program.args.length === 0) {
+  program.help();
 }
 
+/**
+ * Output Helpers
+ */
 
-function downloadFont(searchedTerm, variants, path) {
-	fontList.getFontByName(searchedTerm, function(err, filteredList){
-		if (err){
-			printError(err);
-			return;
-		}
-		if (filteredList.data.length === 1) {
-			filteredList.getFirst().saveAt(variants, path, printResult);
-		} else {
-			console.log('Download failed: unable to find font family "%s". \n'.bold.red, searchedTerm);
-			searchFont(searchedTerm);
-		}
-	})
+function handleMatchError(action, term, err) {
+  if (err) {
+    console.error(pc.red(err.toString()));
+  } else {
+    console.log(
+      pc.bold(pc.red(`${action} failed: unable to find font family "${term}"`))
+    );
+    fontList.searchFontByName(term, printFontList);
+  }
 }
 
-function installFont(searchedTerm, variants) {
-	fontList.getFontByName(searchedTerm, function(err, filteredList){
-		if (err){
-			printError(err);
-			return;
-		}
-		if (filteredList.data.length === 1) {
-			filteredList.getFirst().install(variants, printResult);
-		} else {
-			console.log('Installation failed: unable to find font family "%s". \n'.bold.red, searchedTerm);
-			searchFont(searchedTerm);
-		}
-	})
-}
+function printFontList(err, list, message = "Search results for:") {
+  if (err) return console.error(pc.red(err.toString()));
+  if (list.data.length === 0) {
+    return console.log(pc.red(`No results found for: ${list._filterTerm}`));
+  }
 
-function copyFont(searchedTerm, variants) {
-	fontList.getFontByName(searchedTerm, function(err, filteredList){
-		if (err){
-			printError(err);
-			return;
-		}
-		if (filteredList.data.length === 1) {
+  console.log(pc.green(`${message} "${pc.bold(pc.blue(list._filterTerm))}"\n`));
 
-			var list 	= filteredList.getFirst();
-			var cssUrl 	= variants ? list.cssUrl + ':' + variants.join(',') : list.cssUrl;
-
-				ncp.copy(cssUrl, function () {
-			        console.log('"%s" font url has been copied to your clipboard.'.green, searchedTerm);
-			    });
-
-		} else {
-			console.log('Copy failed: unable to find font family "%s". \n'.bold.red, searchedTerm);
-			searchFont(searchedTerm);
-		}
-	})
-}
-
-function printFontList(err, list, message){
-	if (err) {
-		printError(err)
-		return;
-	} else if (list.data.length === 0) {
-		console.log('No results found for: %s\n'.red, list._filterTerm);
-	} else {
-		message = message || 'Search results for:'
-		if (list._filterTerm)
-			console.log('%s "%s"\n'.green, message, (list._filterTerm || '').bold.blue);
-		list.data.forEach(function(el){
-			console.log(" * %s".bold.blue,  el.family);
-			console.log("    Category: %s\n    Variants: %s\n    CSS Url: %s\n", el.getCategory(), el.getVariants().join(", "), el.getCssUrl());
-		})
-	}
+  list.data.forEach((el) => {
+    console.log(pc.bold(pc.blue(` * ${el.family}`)));
+    console.log(`    Category: ${el.getCategory()}`);
+    console.log(`    Variants: ${el.getVariants().join(", ")}`);
+    console.log(`    CSS Url:  ${el.getCssUrl()}\n`);
+  });
 }
 
 function printResult(err, result) {
-	if (err) {
-		printError(err);
-		return;
-	}
-	console.log('');
-	result.forEach(function(el){
-		console.log('%s variant %s downloaded in %s'.green, (el.family || '??').bold, (el.variant || '??').bold, (el.path || '??').underline);
-	})
-	console.log('');
-}
-
-function printError(err) {
-    console.error('Error, please try again!'.bold.red);
-	console.error(err.toString().bold.red);
-	process.exit(1);
+  if (err) return console.error(pc.red(err.toString()));
+  console.log("");
+  result.forEach((el) => {
+    console.log(
+      pc.green(
+        `${pc.bold(el.family)} variant ${pc.bold(el.variant)} processed: ${pc.underline(el.path)}`
+      )
+    );
+  });
+  console.log("");
 }
